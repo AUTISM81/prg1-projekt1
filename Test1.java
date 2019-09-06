46ed4debdf980ce3847da862b064b1209e565089
Christian Berggren Bäckström <christian.backstrom@learnet.se>
2019-09-06 10:37:29 +0200

    prg1-prac

---
+++ Test1.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import sun.audio.*;
+
+public class Test1{
+	public static void main (String[] args){
+		JOptionPane.showMessageDialog(null,"Error " + "\nYou are gay " +
+			System.getProperty("user.name") + "!");
+				Toolkit.getDefaultToolkit().beep();
+		JOptionPane.showMessageDialog(null,"Tel: 070 396 01 00" + "\nAdress: Nasfallan 5, Grasmyr");
+			Toolkit.getDefaultToolkit().MARIO DIES  SOUND EFFECT.mp3();
+			String gongFile = "/C:\Code\Web1\visual studio code\ljudMARIO DIES  SOUND EFFECT.mp3";
+    InputStream in = new FileInputStream(gongFile);
+
+	}
+}
