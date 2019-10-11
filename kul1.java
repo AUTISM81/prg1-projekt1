5bebc5ec9357ffe3416cdd46bab174dfc9cba80b
Christian Berggren Bäckström <christian.backstrom@learnet.se>
2019-10-11 11:16:19 +0200

    lektion 11/10

--- kul1.java
+++ kul1.java
@@ -1,18 +1,26 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class kul1 {
 public static void main (String [] args){
-	for (p = 0 ; p++) {
-		
-		for (d = 1 ; d++) {
-
-			if ( p%d == 0) {
-				 
-				}
-			int 
-			if (d >= 2) {
-				
-			}
-			}
-		System.out.println(p)
-		}
+	//variabler
+	long summa;
+	int k = 0;
+	int u = 0;
+	int o = 1
+	
+	//scanner
+	Scanner input = new Scanner (System.in);
+	String N1 = JOptionPane.showInputDialog(null, "Please enter choosen number");
+	int n = Integer.parseInt(N1);
+	
+	//koden
+	
+	while (k <= n) {
+		summa = o/k;
+		u = (k+1)*(k+1);
+		k = k + 1;
+	}
+	JOptionPane.showMessageDialog(null, "din summa = " + summa);
 	}
 }
